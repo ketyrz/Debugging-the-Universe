6da0518710b64cb8ec20e9e4c6433a50e37174f9
Ketlyn Rodrigues Rayzel <kety.rayzel@gmail.com>
2025-06-12 13:18:25 -0300

    Adicionando movimento para baixo e colisores ao redor das fases

--- Scripts/Foguete.gd
+++ Scripts/Foguete.gd
@@ -11,6 +11,8 @@ func _physics_process(delta):
 
 	if Input.is_action_pressed("ui_up"):
 		direction.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
 	if Input.is_action_pressed("ui_left"):
